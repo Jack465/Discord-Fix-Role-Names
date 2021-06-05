d41a798e31a0d12114db4da761723270c8c9e6b4
Jack Scott <Jack465@users.noreply.github.com>
2021-06-05 20:06:10 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// From https://stackoverflow.com/a/5574446
 String.prototype.toProperCase = function() {
     return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
